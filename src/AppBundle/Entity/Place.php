2d7b88a708f724250009762aa1789314bbab6aa7
Яyan Priebe <rpriebe@me.com>
2015-11-18 17:10:51 -0700

    adding mostly complete place model/entity

---
+++ src/AppBundle/Entity/Place.php
@@ -0,0 +1,64 @@
+<?php
+
+	namespace AppBundle\Entity;
+
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\Validator\Constraints as Assert;	
+
+	class Place {
+		protected $places = array();
+		
+		/**
+		 * @Assert\NotBlank();
+		 */
+		protected $api_key;
+
+		/**
+		 * @Assert\NotBlank();
+		 */
+		protected $query;
+
+		public function getApiKey(){
+			return $this->api_key;
+		}
+
+		public function setApiKey($value){
+			$this->api_key = $value;
+		}
+
+		public function getQuery(){
+			return $this->query;
+		}
+
+		public function setQuery($value){
+			$this->query = $value;
+		}
+
+		public function getPlaces(){
+			$url = sprintf('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=53.55014,-113.46871&types=food|cafe|drink&keyword=%s&radius=5000&key=%s',
+				$this->query,
+				$this->api_key
+				);
+			$ch = curl_init(htmlspecialchars($url));
+
+			curl_setopt($ch, CURLOPT_HEADER, 0);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			$request = json_decode(curl_exec($ch));
+
+			if($request && $request->status == 'OK'){
+				$this->places = $request->results;
+			}
+			
+			curl_close($ch);
+			
+			return $this->places;
+		}
+
+		public function setPlaces($value){
+			return null;
+		}
+	}
+
+?>
