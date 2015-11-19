cb5ceb6343e8de2c357b6d4f483bbde89138fe2a
Яyan Priebe <rpriebe@me.com>
2015-11-18 17:09:00 -0700

    init is deprecated, moving timezone setting to constructor

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,8 +5,14 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+    public function __construct($env, $debug){
+        date_default_timezone_set("America/Edmonton");
+        parent::__construct($env, $debug);
+    }
+
     public function registerBundles()
     {
+
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -32,10 +38,5 @@ class AppKernel extends Kernel
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
-    }
-
-    public function init(){
-        date_default_timezone_set("America/Edmonton");
-        parent::init();
     }
 }
